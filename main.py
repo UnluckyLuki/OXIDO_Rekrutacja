af2849e5fd5f886c0b766530f7a766790b8aea4e
UnluckyLuki <themrluki43@gmail.com>
2024-11-13 15:40:38 +0100

    First prompt and saving to file checking if ai generates valid HTML code

--- main.py
+++ main.py
@@ -33,8 +33,15 @@ def read_content():
     file.close()
     return content
 
+def save_response_to_html_file(content_to_save):
+    with open("artykul.html", "w", encoding="utf-8") as file:
+        file.write(content_to_save)
 
 if __name__ == '__main__':
     content = read_content()
-    response = ai_response("print: this is test")
-    print(response.choices[0].message.content)
+    prompt = "Create ONLY plain HTML code without css or java scripts for a website based on this text: " + content
+    response = ai_response(prompt)
+    content_to_save = response.choices[0].message.content
+    print(content_to_save)
+    save_response_to_html_file(content_to_save)
+
