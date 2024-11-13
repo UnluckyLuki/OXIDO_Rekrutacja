43088a823e17257debf73657ff1c2f551f2b3553
UnluckyLuki <themrluki43@gmail.com>
2024-11-13 15:25:59 +0100

    Adding and testing function responsible for connection and getting response from openai api

--- main.py
+++ main.py
@@ -1,3 +1,31 @@
+import os
+from dotenv import load_dotenv
+import openai
+from openai import OpenAI
+
+load_dotenv()
+
+api_key = os.getenv("API_KEY")
+org = os.getenv("ORG")
+project_id = os.getenv("PROJECT_ID")
+
+client = OpenAI(
+    organization=org,
+    project=project_id,
+    api_key=api_key,
+)
+
+def ai_response(prompt):
+    response = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": prompt,
+            }
+            ],
+        model="gpt-4o-mini",
+    )
+    return response
 
 def read_content():
     file = open("Zadanie dla JJunior AI Developera - tresc artykulu.txt", "r")
@@ -8,4 +36,5 @@ def read_content():
 
 if __name__ == '__main__':
     content = read_content()
-    print(content)
+    response = ai_response("print: this is test")
+    print(response.choices[0].message.content)
